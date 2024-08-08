ac399021bbda258619f25650897a73d67690b486
Emerson Marques Pedro <emersonmp@gmail.com>
2024-08-08 20:09:01 -0300

    Update

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,14 +1,20 @@
-// components/Contato.js
-
-import React from "react";
+import React, { useEffect } from "react";
 
 function Contato() {
+  useEffect(() => {
+    const emailElement = document.getElementById("email");
+    if (emailElement) {
+      emailElement.textContent = "emerson.pedro@alumni.usp.br";
+    }
+  }, []);
+
   return (
     <section id="contato" className="py-5">
       <div className="container">
         <h2>Contato</h2>
-
-        <p>Envie um e-mail para emerson.pedro[at]alumni.usp.br</p>
+        <p>
+          Envie um e-mail para <span id="email"></span>
+        </p>
       </div>
     </section>
   );
