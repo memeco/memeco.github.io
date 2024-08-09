a187f89cb638a123edfe3e0830a3f657336ddbeb
Emerson Marques Pedro <emersonmp@gmail.com>
2024-08-09 06:49:16 -0300

    Correcting contacts

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -5,7 +5,7 @@ function Contato() {
     <section id="contato" className="py-5">
       <div className="container">
         <h2>Contato</h2>
-        <p>Envie um e-mail para emerson.pedro[at]alumni[dot]uspbr</p>
+        <p>Envie um e-mail para emerson.pedro[at]alumni[dot]usp[dot]br</p>
       </div>
     </section>
   );
